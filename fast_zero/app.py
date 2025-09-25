7d51f2c3088851e089bd15a969d193cb01bac0c2
RonaldoSiqueira <ronaldo.edicassio@gmail.com>
2025-09-25 15:19:48 -0300

    exercicios

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,35 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI,HTTPException
+from fastapi.responses import HTMLResponse
+from http import HTTPStatus
+from pydantic import UserPublic
 app = FastAPI()
 
 
 @app.get('/')
 def read_root():
     return {'message': 'Olá Mundo'}
+
+@app.get('/exercicio-html', response_class=HTMLResponse)
+def exercicio_aula_02():
+    return """
+    <html>
+      <head>
+        <title>Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
+
+@app.get('/users/{user_id}', response_model=UserPublic)
+def read_user(user_id: int):
+    # Busca o usuário pelo ID em toda a lista
+    user = next((user for user in database if user.get('id') == user_id), None)
+    
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.NOT_FOUND, 
+            detail='User not found'
+        )
+    
+    return user
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,17 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
     responde = client.get('/')  # Act (ação)
     assert responde.status_code == HTTPStatus.OK  # Assert (afirmação)
     assert responde.json() == {'message': 'Olá Mundo'}
+
+
+def test_exercicio_aula_02_deve_retornar_html_com_ola_mundo():
+    client = TestClient(app)  # Arrange (organização)
+    response = client.get('/exercicio-html')  # Act (ação)
+    assert response.status_code == HTTPStatus.OK  # Assert (afirmação)
+    assert '<h1> Olá Mundo </h1>' in response.text
+
+
+def test_upadade_user_deve_retornar_404_quando_usuario_nao_existir():
+    client = TestClient(app)  # Arrange (organização)
+    response = client.put('/users/999', json={'name': 'Novo Nome'})  # Act (ação)
+    assert response.status_code == HTTPStatus.NOT_FOUND  # Assert (afirmação)
+    assert response.json() == {'detail': 'User not found'}
